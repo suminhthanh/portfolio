b5add03472bd4c556dd00b62cab84503726a6538
phlong <long.phan@axonactive.com>
2019-08-27 14:43:41 +0700

    Updates

---
+++ precache-manifest.89c753e535d26abf643a5625d2ec4537.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "047c34f464bdbb44632b902d0e2575a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69bbbeced8e4b5387345",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "79bc050c44b3a73376f4",
+    "url": "/static/js/2.3b499a0a.chunk.js"
+  },
+  {
+    "revision": "69bbbeced8e4b5387345",
+    "url": "/static/js/main.2cc5d1fe.chunk.js"
+  },
+  {
+    "revision": "c49018507a564c0ada45",
+    "url": "/static/js/runtime~main.b908f969.js"
+  }
+]);
