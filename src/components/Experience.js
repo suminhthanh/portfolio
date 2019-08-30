bdeff9ffd243a84cdadd0a80e608c5dcf63ea962
hoanglonglch <hoanglonglch@gmail.com>
2019-08-30 22:24:54 +0700

    Change content

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -17,10 +17,7 @@ class Experience extends Component {
                         <div className="resume-content">
                             <h3 className="mb-0">Full stack Developer</h3>
                             <div className="subheading mb-3">Axon Active</div>
-                            <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end
-                                of the day, going forward, a new normal that has evolved from generation X is on the
-                                runway heading towards a streamlined cloud solution. User generated content in real-time
-                                will have multiple touchpoints for offshoring.</p>
+                            <p>Analyze business process based on the idea from customer. Work with Maven, Spring framework on server side and modern JS framework client side.</p>
                         </div>
                         <div className="resume-date text-md-right">
                             <span className="text-primary">Jun 2019 - Present</span>
@@ -31,10 +28,8 @@ class Experience extends Component {
                         <div className="resume-content">
                             <h3 className="mb-0">Web Developer</h3>
                             <div className="subheading mb-3">Enclave</div>
-                            <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test.
-                                Override the digital divide with additional clickthroughs from DevOps. Nanotechnology
-                                immersion along the information highway will close the loop on focusing solely on the
-                                bottom line.</p>
+                            <p>Enclave is an ODC environment, I have the opportunity to work directly with client oversea to get user requirement, analyze and build products.
+                                Besides that, I'm also an Android mentor who support newcomers for quickly adapt to everything related to Java, Android, Coding style and Teamwork process.</p>
                         </div>
                         <div className="resume-date text-md-right">
                             <span className="text-primary">Jul 2017 - May 2019</span>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,33 +1,73 @@
 import React, {Component} from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faCheckCircle} from "@fortawesome/free-solid-svg-icons";
+import {faHtml5, faGithub, faCss3, faJava, faJs, faReact, faNodeJs, faAngular} from "@fortawesome/free-brands-svg-icons";
+
 
 
 class Skills extends Component {
     constructor(props) {
         super(props);
-
         this.skills = props.skills;
     }
 
+    showIcon(iconName){
+        console.log('icon name', iconName);
+        switch (iconName) {
+            case "html5": return faHtml5;
+            case "css3": return faCss3;
+            case "java": return faJava;
+            case "js": return faJs;
+            case "react": return faReact;
+            case "node-js": return faNodeJs;
+            case "angular": return faAngular;
+            case "github": return faGithub;
+        }
+    }
+
+
     render() {
+        console.log('data1', this.showIcon("html5"));
         return (
             <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="skills">
                 <div className="w-100">
                     <h2 className="mb-5">Skills</h2>
+
                     <div className="subheading mb-3">Programming Languages &amp; Tools</div>
-                    <div className="row">
-                        {
-                            this.skills.map((data, index) => (
-                                <div key={index} className="col-6">
-                                    <p className="list-item">
-                                        <FontAwesomeIcon icon={faCheckCircle} color="green" />
-                                        <span className="ml-3">{data.name}</span>
-                                    </p>
-                                </div>
-                            ))
-                        }
-                    </div>
+                    <ul className="list-inline dev-icons">
+
+                        {this.skills.map((s, i) => {
+                            return (
+                                <li key={i} className="list-inline-item pointer-event">
+                                    <i>
+                                        <FontAwesomeIcon style={{cursor: 'pointer'}} icon={this.showIcon(s.iconName)}/>
+                                    </i>
+                                </li>
+
+                            );
+                        })}
+
+                    </ul>
+
+                    <div className="subheading mb-3">Workflow</div>
+                    <ul className="fa-ul mb-0">
+                        <li>
+                            <i className="fa-li fa fa-check"></i>
+                            Mobile-First, Responsive Design
+                        </li>
+                        <li>
+                            <i className="fa-li fa fa-check"></i>
+                            Cross Browser Testing &amp; Debugging
+                        </li>
+                        <li>
+                            <i className="fa-li fa fa-check"></i>
+                            Cross Functional Teams
+                        </li>
+                        <li>
+                            <i className="fa-li fa fa-check"></i>
+                            Agile Development &amp; Scrum
+                        </li>
+                    </ul>
                 </div>
             </section>
 
