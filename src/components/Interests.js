3fac28d2e0c6daaf41a45a0c72501891d6262a8b
hoanglonglch <hoanglonglch@gmail.com>
2019-08-30 22:40:26 +0700

    Change content

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -12,7 +12,7 @@ class Interests extends Component {
             <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="interests">
                 <div className="w-100">
                     <h2 className="mb-5">Interests</h2>
-                    <p>Play everything but still bad</p>
+                    <p>Good at with activities outdoor.</p>
                     <p className="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and
                         television shows, I am an aspiring chef, and I spend a large amount of my free time exploring
                         the latest technology advancements in the front-end web development world.</p>
